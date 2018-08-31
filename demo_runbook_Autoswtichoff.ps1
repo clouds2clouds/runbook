1c7d8ba21bd0fe844b8c121acaabe890971231cb
clouds2clouds <42399280+clouds2clouds@users.noreply.github.com>
2018-08-31 11:55:52 +1000

    Created demo_runbook_Autoswtichoff.ps1 in AzureAutomation.

---
+++ demo_runbook_Autoswtichoff.ps1
@@ -0,0 +1,15 @@
+# Parameter input for RG
+Param(
+    [Parameter(Mandatory=$true)]
+    String $rgname
+)
+
+
+# Connect to Azure
+
+
+
+# Find all VMs that match the resource group name
+
+
+# Switch off the VMs
